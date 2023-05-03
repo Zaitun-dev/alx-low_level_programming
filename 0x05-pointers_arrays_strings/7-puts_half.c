656d1f74f3c87dae2aa585f6004135593b9d1cb8
Zaitun-dev <zaitunishaq12@gmail.com>
2023-05-03 15:51:01 -0700

    put and put half

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+* puts_half - Prints the second half of a string.
+* @str: Pointer to the string to be printed.
+*
+* Return: void
+*/
+void puts_half(char *str)
+{
+int i, length = 0, mid;
+while (*(str + length) != '\0')
+length++;
+if (length % 2 == 0)
+mid = length / 2;
+else
+mid = (length - 1) / 2;
+for (i = mid; i < length; i++)
+_putchar(*(str + i));
+_putchar('\n');
+}
