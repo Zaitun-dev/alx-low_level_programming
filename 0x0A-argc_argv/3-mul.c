1700f785f3ecef57f234feb8ceccc7b5c695e1a8
Zaitun-dev <zaitunishaq12@gmail.com>
2023-05-12 17:56:10 -0700

    Multiplication of the two numbers passed as argcs

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,14 +1,11 @@
 #include <stdio.h>
-
 #include "main.h"
 
 /**
-
  * _atoi - converts a string to an integer
  * @s: string to be converted
  *
  * Return: the int converted from the string
-
  */
 
 int _atoi(char *s)
@@ -16,15 +13,10 @@ int _atoi(char *s)
 	int i, d, n, len, f, digit;
 
 	i = 0;
-
 	d = 0;
-
 	n = 0;
-
 	len = 0;
-
 	f = 0;
-
 	digit = 0;
 
 	while (s[len] != '\0')
@@ -37,42 +29,30 @@ int _atoi(char *s)
 		if (s[i] == '-')
 
 			++d;
-
 		if (s[i] >= '0' && s[i] <= '9')
 
 		{
 			digit = s[i] - '0';
-
 			if (d % 2)
 
 				digit = -digit;
 
 			n = n * 10 + digit;
-
 			f = 1;
 
 			if (s[i + 1] < '0' || s[i + 1] > '9')
-
 				break;
-
 			f = 0;
 		}
-
 		i++;
 	}
-
 	if (f == 0)
 
 		return (0);
-
 	return (n);
-
 }
 
-
-
 /**
-
  * main - multiplies two numbers
  * @argc: number of arguments
  * @argv: array of arguments
@@ -94,13 +74,9 @@ int main(int argc, char *argv[])
 	}
 
 	num1 = _atoi(argv[1]);
-
 	num2 = _atoi(argv[2]);
-
 	result = num1 * num2;
-
 	printf("%d\n", result);
-
 	return (0);
 }
 
