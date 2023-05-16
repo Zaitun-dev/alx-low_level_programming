029dac8cfb0b1e08f8062638766ec23a5f122d23
Zaitun-dev <zaitunishaq12@gmail.com>
2023-05-16 13:57:03 -0700

    quick fix

--- 0x09-static_libraries/1-strncat.c
+++ 0x09-static_libraries/1-strncat.c
@@ -1,23 +1,30 @@
 #include "main.h"
 
 /**
-* _strncat - Concatenates two strings
-* @dest: Pointer to destination string
-* @src: Pointer to source string
-* @n: Maximum number of bytes to be used from src
+* _strncat - concatenate two strings
+* using at most n bytes from src
+* @dest: input value
+* @src: input value
+* @n: input value
 *
-* Return: Pointer to resulting string dest
+* Return: dest
 */
 char *_strncat(char *dest, char *src, int n)
 {
-int dest_len = 0;
 int i;
-while (dest[dest_len] != '\0')
+int j;
+i = 0;
+while (dest[i] != '\0')
 {
-dest_len++;
+i++;
 }
-for (i = 0; i < n && src[i] != '\0'; i++)
+j = 0;
+while (j < n && src[j] != '\0')
 {
-dest[dest_len] = src[i];
-dest_len++;
+dest[i] = src[j];
+i++;
+j++;
+}
+dest[i] = '\0';
+return (dest);
 }
