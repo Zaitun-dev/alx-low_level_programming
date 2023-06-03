f11b143557f817b0341e9614ae9be9f208ec7de3
Zaitun-dev <zaitunushaq12@gmail.com>
2023-06-02 17:57:09 -0700

    hot fix

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -10,18 +10,15 @@
 size_t print_list(const list_t *h)
 {
 size_t count = 0;
-while (h!= NULL)
+while (h)
 {
-if (h->str == NULL)
-{
-printf("[%ld] (nil)\n", count);
-}
+if (!h->str)
+printf("[0] (nil)\n");
 else
-{
-printf("[%ld] %s\n", count, h->str);
-}
-count++;
+printf("[%u] %s\n", h->len, h->str);
 h = h->next;
+count++;
 }
-return count;
+return (count);
 }
+
