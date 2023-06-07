aabb19aabb9a4f450501d48505fdb28e9d4cb47a
Zaitun-dev <zaitunushaq12@gmail.com>
2023-06-06 17:08:47 -0700

    comiting all

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -11,10 +11,14 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
 unsigned int i = 0;
 listint_t *current = head;
-while (current && i < index)
+while (current != NULL)
 {
-current = current->next;
+if (i == index)
+{
+return current;
+}
 i++;
+current = current->next;
 }
 return (NULL);
 }
